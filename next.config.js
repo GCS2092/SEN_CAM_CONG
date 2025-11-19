76f8abca43fd171badd6d1497aaf8437dfedfe4c
GCS2092 <slovengama05@gmail.com>
2025-11-19 08:08:26 +0000

    fix: Ajouter tous les patterns Vercel Blob Storage pour corriger l'erreur d'upload vidéo

--- next.config.js
+++ next.config.js
@@ -8,43 +8,59 @@ const withPWA = require('next-pwa')({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**.cloudinary.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.youtube.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.ytimg.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'img.youtube.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.public.blob.vercel-storage.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.blob.vercel-storage.com',
-      },
-    ],
-    unoptimized: false,
-  },
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: '**.cloudinary.com',
+        },
+        {
+          protocol: 'https',
+          hostname: '**.youtube.com',
+        },
+        {
+          protocol: 'https',
+          hostname: 'i.ytimg.com',
+        },
+        {
+          protocol: 'https',
+          hostname: 'img.youtube.com',
+        },
+        {
+          protocol: 'https',
+          hostname: 'images.unsplash.com',
+        },
+        {
+          protocol: 'https',
+          hostname: 'res.cloudinary.com',
+        },
+        {
+          protocol: 'https',
+          hostname: '**.public.blob.vercel-storage.com',
+        },
+        {
+          protocol: 'https',
+          hostname: '**.blob.vercel-storage.com',
+        },
+        {
+          protocol: 'https',
+          hostname: '*.public.blob.vercel-storage.com',
+        },
+        {
+          protocol: 'https',
+          hostname: '*.blob.vercel-storage.com',
+        },
+        {
+          protocol: 'https',
+          hostname: 'public.blob.vercel-storage.com',
+        },
+        {
+          protocol: 'https',
+          hostname: 'blob.vercel-storage.com',
+        },
+      ],
+      unoptimized: false,
+    },
 }
 
 module.exports = withPWA(nextConfig)
